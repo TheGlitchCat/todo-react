2387fc6c8288e1edbf4625bf4213bf65113d8f12
Dreivko <jorgesebasah@hotmail.com>
2019-05-28 01:36:39 -0500

    Updates

---
+++ precache-manifest.7588f90f580497043dbd468070496d8b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb1a287728f7e8a547c09e2ae6057a32",
+    "url": "/todo-react/index.html"
+  },
+  {
+    "revision": "6ad88345457b9d07cb65",
+    "url": "/todo-react/static/css/main.93a056d5.chunk.css"
+  },
+  {
+    "revision": "9affbd6b2a8b45346067",
+    "url": "/todo-react/static/js/2.86274400.chunk.js"
+  },
+  {
+    "revision": "6ad88345457b9d07cb65",
+    "url": "/todo-react/static/js/main.032106b4.chunk.js"
+  },
+  {
+    "revision": "cd203911fc85a7f73b39",
+    "url": "/todo-react/static/js/runtime~main.6292bb99.js"
+  }
+]);
